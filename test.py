5be6cec918752c429a9749977b6a7b66bf3b2cfa
Gyan Lakhwani <gyanlakhwani@gmail.com>
2019-03-10 23:45:52 +0530

    Get foundry name, weight names

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-//Given a myfonts URL, extract all the tags.
+#Given a myfonts URL, extract all the tags.
 
 from bs4 import BeautifulSoup
 
@@ -11,5 +11,18 @@ response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
 type(html_soup)
 
+#Code for printing font weights
+print("Foundry, Weights : ", end=" ")
+
+for link in html_soup.find_all('h4'):
+    print(link.a.text, end=", ")
+
+print("")
+
+#Code for printing tags
+print("Tags : ", end=" ")
+
 for link in html_soup.find_all('span', class_ = 'tag'):
-    print(link.text)
+    print(link.text, end=", ")
+
+print("")
